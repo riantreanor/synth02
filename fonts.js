d623016e1ddc00f2ca2b8c52df50b7bdc47be0f0
riantreanor <74061047+riantreanor@users.noreply.github.com>
2020-11-08 20:31:13 +0000

    Deploying to gh-pages from master @ 6fddcc41486300db8a685865278a854a117ee1e0 🚀

--- fonts.js
+++ fonts.js
@@ -171,7 +171,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/MicrochipRegular-ExWg.ttf", "start": 145348, "end": 168788, "audio": 0}], "remote_package_size": 168788, "package_uuid": "95cfa924-d0ae-4b7e-ae97-c61289b2dd39"});
+   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/MicrochipRegular-ExWg.ttf", "start": 145348, "end": 168788, "audio": 0}], "remote_package_size": 168788, "package_uuid": "c3d6ce3c-989f-4189-bba4-24fe3ddd73e3"});
   
   })();
   
